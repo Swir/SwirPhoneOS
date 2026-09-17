bd30bf14c4b86e21f67f907347fb1571acfaaf54
Wojciech <swirjestem@hotmail.com>
2026-09-17 14:35:48 +0200

    build(gsi): register ARM64 GSI lunch target

--- platform/aosp_product/AndroidProducts.mk
+++ platform/aosp_product/AndroidProducts.mk
@@ -1,8 +1,10 @@
-# SwirPhoneOS Cuttlefish developer product registration.
-# This file is intended to live under device/swir/phone in a pinned AOSP tree.
+# SwirPhoneOS developer product registration.
+# These sources are intended to live under vendor/swir/products in a pinned AOSP tree.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/swirphoneos_cf_x86_64.mk
+    $(LOCAL_DIR)/swirphoneos_cf_x86_64.mk \
+    $(LOCAL_DIR)/swirphoneos_gsi_arm64.mk
 
 COMMON_LUNCH_CHOICES := \
-    swirphoneos_cf_x86_64-aosp_current-userdebug
+    swirphoneos_cf_x86_64-aosp_current-userdebug \
+    swirphoneos_gsi_arm64-aosp_current-userdebug
